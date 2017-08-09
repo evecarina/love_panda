95567126e9cd73865665af85c497a59281602ba9
yudithchuctaya <yudithyesicachuctaya@gmail.com>
2017-08-09 10:56:56 -0500

    sacandolo de la carpeta y poniendolo todo en la misma posicion

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+var pictures = ["assets/img/a1.jpg",
+                "assets/img/a2.jpg",
+                "assets/img/a3.jpg",
+                "assets/img/a4.jpg"];
+
+var section = document.getElementById('pictures');
+for (var i = 0; i < pictures.length; i++) {
+  var photo = document.createElement('img');
+  photo.setAttribute('src', pictures[i]);
+  photo.setAttribute('class', 'photo');
+  section.appendChild(photo);
+}
